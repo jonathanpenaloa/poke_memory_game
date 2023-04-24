3a740a6b7411432f69a3bb8d0b430603a51a2e3e
Jonathan Pena-Loa <jpena-loa@LABA.PSWAS.LOCAL>
2023-04-24 13:36:38 -0700

    layout roadmap for code

---
+++ main.js
@@ -0,0 +1,32 @@
+/*----- constants -----*/
+
+/* pokemon (name / img) */
+
+
+/*----- state variables -----*/
+  // turn
+  // board
+  // messge
+  // points
+  // cardCollection
+  // winner
+
+/*----- cached elements  -----*/
+
+/* borad */ 
+/* playerScore */ 
+/* computerScore */ 
+ 
+
+
+
+
+/*----- event listeners -----*/
+
+/* board.addeventLis */
+/* playAgainButton */
+
+
+
+
+/*----- functions -----*/
