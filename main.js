bad68b74620eb8af7f734fb254cc2c6cc9bd28ed
Jonathan Pena-Loa <jpena-loa@LABA.PSWAS.LOCAL>
2023-04-24 15:52:09 -0700

    implemented fecthing API for  pokemon information

--- main.js
+++ main.js
@@ -2,6 +2,51 @@
 
 /* pokemon (name / img) */
 
+// const playingPokemon = []
+
+
+
+// for (let i=1; i<20; i++) {
+//     const getPokemonCount = Math.floor(Math.random() * 90)
+//     const url = `https://pokeapi.co/api/v2/pokemon/${getPokemonCount}`
+//     const getPokemon = async () => {
+//         const responds = await fetch(url)
+//         const obj = await responds.json()
+//         playingPokemon.push(obj);
+//     }   
+//     getPokemon()
+// }
+// console.log(playingPokemon);
+const populatePokemonArr = async () => {
+    const promises = [];
+    for (let i=1; i<=20; i++) {
+        const getPokemonCount = Math.floor(Math.random() * 90) + 1;
+        const url = `https://pokeapi.co/api/v2/pokemon/${getPokemonCount}`
+        promises.push(fetch(url))
+    }
+    console.log(promises);
+    let pokemonResponses = await Promise.all(promises);
+    let jsonPromises = []
+    pokemonResponses.forEach((responseObject) => {
+        jsonPromises.push(responseObject.json())
+    })
+    console.log(jsonPromises);
+    let finalPokemonData = await Promise.all(jsonPromises)
+    console.log(finalPokemonData);
+    // here you actually want to just create an array of the form[0]
+    return finalPokemonData
+}
+const playGame = async () => {
+    const playingPokemon = await populatePokemonArr()
+    console.log(playingPokemon);
+
+    const nameAndImage = []
+
+}
+playGame()
+
+
+
 
 /*----- state variables -----*/
   // turn
